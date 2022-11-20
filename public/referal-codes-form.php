24cb8f9b4271da34e004a2512cee68e435d410ff
jayaprasad356 <jayaprasad356@gmail.com>
2022-11-21 03:14:36 +0530

    referral bonus included in referal code

--- public/referal-codes-form.php
+++ public/referal-codes-form.php
@@ -7,6 +7,7 @@ $fn = new custom_functions;
 if (isset($_POST['btnUpdate'])) {
     $mobile = $db->escapeString(($_POST['mobile']));
     $referred_by = $db->escapeString(($_POST['referred_by']));
+    $datetime = date('Y-m-d H:i:s');
     $error = array();
 
     if (!empty($mobile) && !empty($referred_by)) {
@@ -15,6 +16,30 @@ if (isset($_POST['btnUpdate'])) {
     $db->sql($sql_query);
     $update_result = $db->getResult();
     if (!empty($update_result)) {
+        $code_bonus = 1000 * COST_PER_CODE;
+        $referral_bonus = 550;
+        $sql_query = "UPDATE users SET `total_referrals` = total_referrals + 1,`earn` = earn + $referral_bonus,`balance` = balance + $referral_bonus WHERE refer_code =  '$referred_by' AND status = 1";
+        $db->sql($sql_query);
+        $sql_query = "SELECT * FROM users WHERE refer_code =  '$referred_by'";
+        $db->sql($sql_query);
+        $res = $db->getResult();
+        $user_id = $res[0]['id'];
+        $num = $db->numRows($res);
+        if ($num == 1){
+            $sql_query = "INSERT INTO transactions (user_id,amount,datetime,type)VALUES($user_id,$referral_bonus,'$datetime','refer_bonus')";
+            $db->sql($sql_query);
+            $code_generate = $res[0]['code_generate'];
+            if($code_generate == 1){
+                $sql_query = "UPDATE users SET `earn` = earn + $code_bonus,`balance` = balance + $code_bonus,`today_codes` = today_codes + 1000,`total_codes` = total_codes + 1000 WHERE refer_code =  '$referred_by' AND code_generate = 1";
+                $db->sql($sql_query);
+                $sql_query = "INSERT INTO transactions (user_id,amount,codes,datetime,type)VALUES($user_id,$code_bonus,1000,'$datetime','code_bonus')";
+                $db->sql($sql_query);
+            }
+            $sql_query = "UPDATE users SET refer_bonus_sent = 1 WHERE id =  $ID";
+            $db->sql($sql_query);
+
+        }
+
         $update_result = 0;
     } else {
         $update_result = 1;
