5427df5e12227aea6b875da0b13e1a9449c7229f
divaharsha <divakarvan03@gmail.com>
2023-01-29 20:38:39 +0530

    valid urls list api created

---
+++ api/valid_urls_List.php
@@ -0,0 +1,39 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+include_once('../includes/crud.php');
+
+$db = new Database();
+$db->connect();
+
+$sql = "SELECT * FROM valid_urls";
+$db->sql($sql);
+$res = $db->getResult();
+$num = $db->numRows($res);
+if ($num >= 1) {
+    foreach ($res as $row) {
+        $temp['id'] = $row['id'];
+        $temp['url'] = $row['url'];
+        $rows[] = $temp;
+        
+    }
+
+    $response['success'] = true;
+    $response['message'] = "Valid URL's listed Successfully";
+    $response['data'] = $rows;
+    print_r(json_encode($response));
+
+}else{
+    $response['success'] = false;
+    $response['message'] = "No URLS Found";
+    print_r(json_encode($response));
+
+}
+
+?>
