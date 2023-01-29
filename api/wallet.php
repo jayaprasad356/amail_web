294f7fb5599f37ff2b2db98b895469717aab8bd6
jayaprasad356 <jayaprasad356@gmail.com>
2023-01-30 03:08:58 +0530

    wallet updated

--- api/wallet.php
+++ api/wallet.php
@@ -28,29 +28,42 @@ $user_id = $db->escapeString($_POST['user_id']);
 $codes = (isset($_POST['codes']) && $_POST['codes'] != "") ? $db->escapeString($_POST['codes']) : 0;
 $datetime = date('Y-m-d H:i:s');
 $type = 'generate';
-if($codes != 0){
-    $amount = $codes * COST_PER_CODE;
-    $sql = "INSERT INTO transactions (`user_id`,`codes`,`amount`,`datetime`,`type`)VALUES('$user_id','$codes','$amount','$datetime','$type')";
+$sql = "SELECT code_generate FROM settings";
+$db->sql($sql);
+$set = $db->getResult();
+$code_generate = $$set[0]['code_generate'];
+if($code_generate == 1){
+    if($codes != 0){
+        $amount = $codes * COST_PER_CODE;
+        $sql = "INSERT INTO transactions (`user_id`,`codes`,`amount`,`datetime`,`type`)VALUES('$user_id','$codes','$amount','$datetime','$type')";
+        $db->sql($sql);
+        $res = $db->getResult();
+    
+        $sql = "UPDATE `users` SET  `today_codes` = today_codes + $codes,`total_codes` = total_codes + $codes,`earn` = earn + $amount,`balance` = balance + $amount WHERE `id` = $user_id";
+        $db->sql($sql);
+    }
+    
+    $sql = "SELECT today_codes,total_codes,balance,code_generate,status  FROM users WHERE id = $user_id ";
     $db->sql($sql);
     $res = $db->getResult();
-
-    $sql = "UPDATE `users` SET  `today_codes` = today_codes + $codes,`total_codes` = total_codes + $codes,`earn` = earn + $amount,`balance` = balance + $amount WHERE `id` = $user_id";
-    $db->sql($sql);
+    
+    $response['success'] = true;
+    $response['message'] = "Code Added Successfully";
+    $response['status'] = $res[0]['status'];
+    $response['balance'] = $res[0]['balance'];
+    $response['today_codes'] = $res[0]['today_codes'];
+    $response['total_codes'] = $res[0]['total_codes'];
+    $response['code_generate'] = $res[0]['code_generate'];
+    $response['status'] = $res[0]['status'];
+    print_r(json_encode($response));
+}
+else{
+  
+    $response['success'] = false;
+    $response['message'] = "Cannot Sync Right Now, Code Generate is turned off";
+    print_r(json_encode($response));
 }
 
-$sql = "SELECT today_codes,total_codes,balance,code_generate,status  FROM users WHERE id = $user_id ";
-$db->sql($sql);
-$res = $db->getResult();
-
-$response['success'] = true;
-$response['message'] = "Code Added Successfully";
-$response['status'] = $res[0]['status'];
-$response['balance'] = $res[0]['balance'];
-$response['today_codes'] = $res[0]['today_codes'];
-$response['total_codes'] = $res[0]['total_codes'];
-$response['code_generate'] = $res[0]['code_generate'];
-$response['status'] = $res[0]['status'];
-print_r(json_encode($response));
 
 
 ?>
