3324277b3e49b5ff2a7fe852a05e5655c5b1fd3b
divaharsha <divakarvan03@gmail.com>
2023-02-24 11:23:06 +0530

    sa withdrawal api updated and sa_refer_count added

--- api/sa_withdrawal.php
+++ api/sa_withdrawal.php
@@ -34,16 +34,31 @@ $amount = $db->escapeString($_POST['amount']);
 $datetime = date('Y-m-d H:i:s');
 
 
-$sql = "SELECT salary_advance_balance,ongoing_sa_balance,refer_balance FROM users WHERE id = $user_id ";
+$sql = "SELECT salary_advance_balance,ongoing_sa_balance,refer_balance,sa_refer_count FROM users WHERE id = $user_id ";
 $db->sql($sql);
 $res = $db->getResult();
 $user_num=$db->numRows($res);
 $salary_advance_balance = $res[0]['salary_advance_balance'];
+$sa_refer_count = $res[0]['sa_refer_count'];
 if($user_num >= 1 ){
         if($amount <= $salary_advance_balance){
+                    if($amount==2000){
+                        $sql="UPDATE `users` SET `sa_refer_count` = sa_refer_count - 10 WHERE `id` = $user_id";
+                        $db->sql($sql);
+                    }
+                    elseif($amount==3000){
+                        $sql="UPDATE `users` SET `sa_refer_count` = sa_refer_count - 15 WHERE `id` = $user_id";
+                        $db->sql($sql);
+                    }
+                    else{
+                        $sql="UPDATE `users` SET `sa_refer_count` = sa_refer_count - 20 WHERE `id` = $user_id";
+                        $db->sql($sql);
+                    }
                     $sql = "UPDATE `users` SET `salary_advance_balance` = salary_advance_balance - $amount,`ongoing_sa_balance` = ongoing_sa_balance + $amount,`refer_balance`=refer_balance + $amount WHERE `id` = $user_id";
                     $db->sql($sql);
                     $sql = "INSERT INTO salary_advance_trans (`user_id`,`refer_user_id`,`amount`,`datetime`,`type`)VALUES('','$user_id','$amount','$datetime','debit')";
+                    $db->sql($sql);
+                    $sql = "UPDATE `repayments` SET `expiry`=1 WHERE `user_id` = $user_id";
                     $db->sql($sql);
 
                     // Calculate EMI due dates
@@ -58,21 +73,23 @@ if($user_num >= 1 ){
 
                     // Add due dates to the database
                     foreach ($due_dates as $due_date) {
-                        $sql = "INSERT INTO repayments (`user_id`, amount, `due_date`, `status`) VALUES ('$user_id', '$due_amount', '$due_date', 0)";
+                        $sql = "INSERT INTO repayments (`user_id`, amount, `due_date`,`status`) VALUES ('$user_id', '$due_amount', '$due_date',0)";
                         $db->sql($sql);
                     }
 
-                    $sql = "SELECT salary_advance_balance,ongoing_sa_balance,refer_balance FROM users WHERE id = $user_id ";
+                    $sql = "SELECT salary_advance_balance,ongoing_sa_balance,refer_balance,sa_refer_count FROM users WHERE id = $user_id ";
                     $db->sql($sql);
                     $res = $db->getResult();
                     $salary_advance_balance = $res[0]['salary_advance_balance'];
                     $ongoing_sa_balance = $res[0]['ongoing_sa_balance'];
                     $refer_balance = $res[0]['refer_balance'];
+                    $sa_refer_count = $res[0]['sa_refer_count'];
                     $response['success'] = true;
                     $response['salary_advance_balance'] = $salary_advance_balance;
                     $response['ongoing_sa_balance'] = $ongoing_sa_balance;
                     $response['refer_balance'] = $refer_balance;
-                    $response['message'] = "Successfully Transfered to refer_balance";
+                    $response['sa_refer_count'] = $sa_refer_count;
+                    $response['message'] = "Withdrawal requested successfully";
                     print_r(json_encode($response));
             
         }
