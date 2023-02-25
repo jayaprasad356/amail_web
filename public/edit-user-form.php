bbe6125b46a4447a3e34ea1269c0349aeb268ccd
divaharsha <divakarvan03@gmail.com>
2023-02-25 10:54:11 +0530

    edit-user-form updated

--- public/edit-user-form.php
+++ public/edit-user-form.php
@@ -67,12 +67,8 @@ if (isset($_POST['btnEdit'])) {
                 }
 
                 $sa_refer_count=$res[0]['sa_refer_count'];
-                if($sa_refer_count<=15){
-                    $salary_advance_balance=200;
-                }
-                else{
-                    $salary_advance_balance=400;
-                }
+                $salary_advance_balance=200;
+              
                 $sql_query = "UPDATE users SET `total_referrals` = total_referrals + 1,`earn` = earn + $referral_bonus,`refer_balance` = refer_balance + $referral_bonus,`salary_advance_balance`=salary_advance_balance +$salary_advance_balance,`sa_refer_count`=sa_refer_count + 1 WHERE id =  $user_id";
                 $db->sql($sql_query);
                 $sql_query = "INSERT INTO transactions (user_id,amount,datetime,type)VALUES($user_id,$referral_bonus,'$datetime','refer_bonus')";
