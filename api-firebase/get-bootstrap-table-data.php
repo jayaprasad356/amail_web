4249d9c701add2c0b305d922ee2c10fc7a1654f5
divaharsha <divakarvan03@gmail.com>
2023-03-16 16:30:03 +0530

    withdrawals table updated

--- api-firebase/get-bootstrap-table-data.php
+++ api-firebase/get-bootstrap-table-data.php
@@ -442,6 +442,10 @@ if (isset($_GET['table']) && $_GET['table'] == 'withdrawals') {
     }
     $join = "WHERE w.user_id = u.id AND w.user_id = b.user_id ";
 
+    // Calculate the date 7 days ago
+    $seven_days_ago = date('Y-m-d', strtotime('-7 days'));
+    $where .= " AND w.datetime >= '$seven_days_ago' ";
+
     $sql = "SELECT COUNT(w.id) as total FROM `withdrawals` w,`users` u,`bank_details` b $join ". $where ."";
     $db->sql($sql);
     $res = $db->getResult();
