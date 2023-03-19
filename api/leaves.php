1da57477b3f4551c30b400af8ea18e7a4ede2782
jayaprasad356 <jayaprasad356@gmail.com>
2023-03-19 19:53:41 +0530

    leaves

--- api/leaves.php
+++ api/leaves.php
@@ -19,8 +19,8 @@ $fn = new functions;
 $date = date('Y-m-d');
 
 
-// $sql = "UPDATE `users` SET `code_generate` = 1,withdrawal_status = 1 WHERE `code_enable_date` = '$date'";
-// $db->sql($sql);
+$sql = "UPDATE `users` SET `code_generate` = 1,withdrawal_status = 1 WHERE `code_enable_date` = '$date'";
+$db->sql($sql);
 
 $sql = "UPDATE `settings` SET `code_generate` = 1 WHERE `id` = 1";
 $db->sql($sql);
@@ -40,27 +40,27 @@ if ($num >= 1) {
 
 
 }else{
-    // $sql = "UPDATE `users` SET `worked_days` = worked_days + 1 WHERE `code_generate` = 1 AND status = 1";
-    // $db->sql($sql);
+    $sql = "UPDATE `users` SET `worked_days` = worked_days + 1 WHERE `code_generate` = 1 AND status = 1";
+    $db->sql($sql);
 
-    // $sql = "SELECT * FROM leaves WHERE date='$date' AND status = 1 AND type = 'user_leave' ORDER BY date";
-    // $db->sql($sql);
-    // $res = $db->getResult();
-    // $num = $db->numRows($res);
+    $sql = "SELECT * FROM leaves WHERE date='$date' AND status = 1 AND type = 'user_leave' ORDER BY date";
+    $db->sql($sql);
+    $res = $db->getResult();
+    $num = $db->numRows($res);
 
-    // if ($num >= 1) {
-    //     foreach ($res as $row) {
-    //         $user_id = $row['user_id'];
-    //         $leave_date = $row['date'];
-    //         $next_date = date('Y-m-d', strtotime($leave_date . ' +1 day'));
-    //         $sql = "UPDATE users SET `worked_days` = worked_days - 1,code_enable_date = '$next_date',code_generate=0,withdrawal_status = 0  WHERE id = $user_id";
-    //         $db->sql($sql);
-    //     }
+    if ($num >= 1) {
+        foreach ($res as $row) {
+            $user_id = $row['user_id'];
+            $leave_date = $row['date'];
+            $next_date = date('Y-m-d', strtotime($leave_date . ' +1 day'));
+            $sql = "UPDATE users SET `worked_days` = worked_days - 1,code_enable_date = '$next_date',code_generate=0,withdrawal_status = 0  WHERE id = $user_id";
+            $db->sql($sql);
+        }
 
-    //     $response['success'] = true;
-    //     $response['message'] = "Leave Updated Successfully";
-    //     print_r(json_encode($response));
-    // }
+        $response['success'] = true;
+        $response['message'] = "Leave Updated Successfully";
+        print_r(json_encode($response));
+    }
 }
 
 
