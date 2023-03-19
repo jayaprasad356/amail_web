fa031ae26d4cbdb137520d0828aaa0f6c26fc127
jayaprasad356 <jayaprasad356@gmail.com>
2023-03-19 18:34:58 +0530

    ratings

--- api/ratings.php
+++ api/ratings.php
@@ -13,43 +13,77 @@ include_once('../includes/crud.php');
 $db = new Database();
 $db->connect();
 
+if($_POST['rate_type'] == 'check'){
+    if (empty($_POST['user_id'])) {
+        $response['success'] = false;
+        $response['message'] = "User Id is Empty";
+        print_r(json_encode($response));
+        return false;
+    }
+    if (empty($_POST['ticket_id'])) {
+        $response['success'] = false;
+        $response['message'] = "Ticket Id is Empty";
+        print_r(json_encode($response));
+        return false;
+    }
+    $user_id = $db->escapeString($_POST['user_id']);
+    $ticket_id = $db->escapeString($_POST['ticket_id']);
+    $sql_query = "SELECT * FROM ratings WHERE user_id =  $user_id AND ticket_id = '$ticket_id'";
+    $db->sql($sql_query);
+    $res = $db->getResult();
+    $num = $db->numRows($res);
+    if ($num >= 1){
+        $response['success'] = false;
+        $response['message'] = "Rated";
+        print_r(json_encode($response));
 
-if (empty($_POST['user_id'])) {
-    $response['success'] = false;
-    $response['message'] = "User Id is Empty";
-    print_r(json_encode($response));
-    return false;
+    }else{
+        $response['success'] = true;
+        $response['message'] = "Not Rated";
+        print_r(json_encode($response));
+    }
+
 }
-if (empty($_POST['employee_id'])) {
-    $response['success'] = false;
-    $response['message'] = "Employee Id is Empty";
+if($_POST['rate_type'] == 'add'){
+    if (empty($_POST['user_id'])) {
+        $response['success'] = false;
+        $response['message'] = "User Id is Empty";
+        print_r(json_encode($response));
+        return false;
+    }
+    if (empty($_POST['employee_id'])) {
+        $response['success'] = false;
+        $response['message'] = "Employee Id is Empty";
+        print_r(json_encode($response));
+        return false;
+    }
+    if (empty($_POST['description'])) {
+        $response['success'] = false;
+        $response['message'] = "Description is Empty";
+        print_r(json_encode($response));
+        return false;
+    }
+    if (empty($_POST['ratings'])) {
+        $response['success'] = false;
+        $response['message'] = "ratings is Empty";
+        print_r(json_encode($response));
+        return false;
+    }
+    $user_id = $db->escapeString($_POST['user_id']);
+    $employee_id = $db->escapeString($_POST['employee_id']);
+    $description = $db->escapeString($_POST['description']);
+    $ratings = $db->escapeString($_POST['ratings']);
+    
+    
+    $sql = "INSERT INTO ratings (`user_id`,`employee_id`,`description`,`ratings`) VALUES ('$user_id','$employee_id','$description','$ratings')";
+    $db->sql($sql);
+    $res = $db->getResult();
+    $response['success'] = true;
+    $response['message'] = "Ratings Submitted Successfully";
     print_r(json_encode($response));
-    return false;
 }
-if (empty($_POST['description'])) {
-    $response['success'] = false;
-    $response['message'] = "Description is Empty";
-    print_r(json_encode($response));
-    return false;
-}
-if (empty($_POST['ratings'])) {
-    $response['success'] = false;
-    $response['message'] = "ratings is Empty";
-    print_r(json_encode($response));
-    return false;
-}
-$user_id = $db->escapeString($_POST['user_id']);
-$employee_id = $db->escapeString($_POST['employee_id']);
-$description = $db->escapeString($_POST['description']);
-$ratings = $db->escapeString($_POST['ratings']);
 
 
-$sql = "INSERT INTO ratings (`user_id`,`employee_id`,`description`,`ratings`) VALUES ('$user_id','$employee_id','$description','$ratings')";
-$db->sql($sql);
-$res = $db->getResult();
-$response['success'] = true;
-$response['message'] = "Ratings Submitted Successfully";
-print_r(json_encode($response));
 
 
 
