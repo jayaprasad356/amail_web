a151acd19e956a70a90d096c65b992c50b73db2a
jayaprasad356 <jayaprasad356@gmail.com>
2023-03-21 23:54:31 +0530

    export withdrawals

--- export-unpaid-withdrawals.php
+++ export-unpaid-withdrawals.php
@@ -9,7 +9,11 @@ $currentdate = date('Y-m-d');
 	$sql = "SELECT w.id AS id,w.*,u.name,u.total_codes,u.total_referrals,u.balance,u.mobile,u.referred_by,u.refer_code,DATEDIFF( '$currentdate',u.joined_date) AS history,b.branch,b.bank,CONCAT(',' , `account_num`, ',') AS account_num,b.ifsc,b.holder_name FROM `withdrawals` w,`users` u,`bank_details` b $join";
 	$db->sql($sql);
 	$developer_records = $db->getResult();
-	
+	$refer_refund=$row['total_referrals'] *250;
+	$code_refund=($row['total_codes']/3000)*100;
+	$total_refund= $refer_refund +  $code_refund;
+	$total_refund = number_format($total_refund, 2);
+	$developer_records['total_refund'] = $total_refund;
 	$filename = "unpaid-withdrawals-data".date('Ymd') . ".xls";			
 	header("Content-Type: application/vnd.ms-excel");
 	header("Content-Disposition: attachment; filename=\"$filename\"");	
