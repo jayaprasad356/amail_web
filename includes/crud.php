0f20d4b90d369af5061e3aabb10a38be6455e987
jayaprasad356 <jayaprasad356@gmail.com>
2023-03-29 09:33:34 +0530

    crud changed

--- includes/crud.php
+++ includes/crud.php
@@ -25,10 +25,16 @@ class Database
 
 
     //LIve
+    // private $db_host = "localhost";
+    // private $db_user = "u117947056_abcd";
+    // private $db_pass = "Abcd@2022"; 
+    // private $db_name = "u117947056_abcd"; 
+
+    
     private $db_host = "localhost";
-    private $db_user = "u117947056_abcd";
-    private $db_pass = "Abcd@2022"; 
-    private $db_name = "u117947056_abcd"; 
+    private $db_user = "u743445510_newabcd";
+    private $db_pass = "Newabcd@01"; 
+    private $db_name = "u743445510_newabcd"; 
 
 
     // private $db_host = "localhost";
