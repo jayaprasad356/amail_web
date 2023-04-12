f7902157061e15f907e26dd1a1c174cff4b67459
jayaprasad356 <jayaprasad356@gmail.com>
2023-04-12 14:44:31 +0530

    demo

--- includes/crud.php
+++ includes/crud.php
@@ -39,10 +39,10 @@ class Database
 
 
 
-     //private $db_host = "localhost";
-     //private $db_user = "root";
-     //private $db_pass = ""; 
-     //private $db_name = "abcd_web"; 
+    //  private $db_host = "localhost";
+    //  private $db_user = "root";
+    //  private $db_pass = ""; 
+    //  private $db_name = "abcd"; 
 
 
     /*
