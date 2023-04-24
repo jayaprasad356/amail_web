623b373e5905cf53dbde30bce9a76b1bab537540
jayaprasad356 <jayaprasad356@gmail.com>
2023-04-24 09:25:46 +0530

    worked days

--- public/edit-user-form.php
+++ public/edit-user-form.php
@@ -98,7 +98,7 @@ if (isset($_POST['btnEdit'])) {
                 $sa_refer_count=$res[0]['sa_refer_count'];
                 $refer_sa_balance=200;
               
-                $sql_query = "UPDATE users SET `total_referrals` = total_referrals + 1,`earn` = earn + $referral_bonus,`balance` = balance + $referral_bonus,`salary_advance_balance`=salary_advance_balance +$refer_sa_balance,`sa_refer_count`=sa_refer_count + 1 WHERE id =  $user_id";
+                $sql_query = "UPDATE users SET `total_referrals` = total_referrals + 1,`worked_days` = worked_days + 1,`earn` = earn + $referral_bonus,`balance` = balance + $referral_bonus,`salary_advance_balance`=salary_advance_balance +$refer_sa_balance,`sa_refer_count`=sa_refer_count + 1 WHERE id =  $user_id";
                 $db->sql($sql_query);
                 $sql_query = "INSERT INTO transactions (user_id,amount,datetime,type)VALUES($user_id,$referral_bonus,'$datetime','refer_bonus')";
                 $db->sql($sql_query);
