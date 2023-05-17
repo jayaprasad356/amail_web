dc9f3e1bff39ac5f1ce5be9aa015e107ce973214
Karthickraj <karthickraj953@gmail.com>
2023-05-17 12:01:20 +0530

    update_remarks.php

--- delete-staff.php
+++ delete-staff.php
@@ -1,8 +1,37 @@
 <?php
+session_start();
+header("Expires: on, 01 Jan 1970 00:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 include_once('includes/crud.php');
 $db = new Database();
 $db->connect();
-	
+// start session
+
+// set time for session timeout
+$currentTime = time() + 25200;
+$expired = 3600;
+
+// if session not set go to login page
+if (!isset($_SESSION['username'])) {
+    header("location:index.php");
+}
+
+// if current time is more than session timeout back to login page
+if ($currentTime > $_SESSION['timeout']) {
+    session_destroy();
+    header("location:index.php");
+}
+
+// destroy previous session timeout and create new one
+unset($_SESSION['timeout']);
+$_SESSION['timeout'] = $currentTime + $expired;
+
+
+if (isset($_POST['btnDelete'])) {
+		
 	if (isset($_GET['id'])) {
 		$ID = $db->escapeString($_GET['id']);
 	} else {
@@ -10,27 +39,44 @@ $db->connect();
 		return false;
 		exit(0);
 	}
-	$data = array();
-	$sql_query = "SELECT *  FROM staffs WHERE id =" . $ID;
+
+	// delete data from menu table
+	$sql_query = "DELETE FROM staffs WHERE id =" . $ID;
 	$db->sql($sql_query);
-	$res = $db->getResult();
-	$target_path = $res[0]['photo'];
-	$target_path1 = $res[0]['resume'];
-	$target_path2 = $res[0]['education_certificate'];
-	$target_path3 = $res[0]['aadhar_card'];
-	if(!empty($target_path) && !empty($target_path1) && !empty($target_path2) && !empty($target_path3)){
-		if(unlink($target_path) && unlink($target_path1) && unlink($target_path2) && unlink($target_path3)){	
-			$sql_query = "DELETE  FROM staffs WHERE id =" . $ID;
-			$db->sql($sql_query);
-			$res = $db->getResult();
-			header("location:staffs.php");
-	    }
-	}
-	else{
-		    $sql_query = "DELETE  FROM staffs WHERE id =" . $ID;
-			$db->sql($sql_query);
-			$res = $db->getResult();
-			header("location:staffs.php");
-	}
-	
+	$delete_result = $db->getResult();
+	header("location: staffs.php");
+}
+
+if (isset($_POST['btnNo'])) {
+header("location: staffs.php");
+}
+if (isset($_POST['btncancel'])) {
+header("location: staffs.php");
+}
 ?>
+
+<?php include "header.php"; ?>
+<html>
+
+<head>
+    <title>Delete staffs | - Dashboard</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+
+<body>
+	
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+	    <h1>Confirm Action</h1>
+		<hr />
+		<form method="post">
+			<p>Are you sure want to delete this staff?</p>
+			<input type="submit" class="btn btn-primary" value="Delete" name="btnDelete" />
+			<input type="submit" class="btn btn-danger" value="Cancel" name="btnNo" />
+			<input type="submit" class="btn btn-warning" value="Back" name="btncancel" />
+		</form>
+    </div><!-- /.content-wrapper -->
+</body>
+
+</html>
+<?php include "footer.php"; ?>
