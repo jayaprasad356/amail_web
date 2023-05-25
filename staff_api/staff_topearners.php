04b5d530a0a35b0e1d084fd9c17e36372ac37ef0
jayaprasad356 <jayaprasad356@gmail.com>
2023-05-25 22:54:24 +0530

    staff top earners

--- staff_api/staff_topearners.php
+++ staff_api/staff_topearners.php
@@ -21,17 +21,31 @@ if (empty($_POST['staff_id'])) {
 }
 $date = date('Y-m-d');
 $staff_id = $db->escapeString($_POST['staff_id']);
-$sql = "SELECT s.name AS name,b.name AS branch_name,SUM(i.amount) AS incentives FROM incentives i JOIN staffs s ON i.staff_id = s.id
-JOIN branches b ON s.branch_id = b.id WHERE s.branch_id = b.id AND s.incentives != 0 AND s.staff_role_id != 1 AND s.staff_role_id != 2 AND YEAR(i.datetime) = YEAR('$date') AND WEEK(i.datetime) = WEEK('$date') ORDER BY SUM(i.amount) DESC LIMIT 10";
+$sql = "SELECT s.id,s.name AS name,b.name AS branch_name,s.incentives FROM staffs s,branches b WHERE s.branch_id = b.id AND s.incentives != 0 AND s.staff_role_id != 1 AND s.staff_role_id != 2 ORDER BY s.incentives DESC LIMIT 5";
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
 
 if ($num >= 1) {
+    foreach ($res as $row) {
+        $staff_id = $row['id'];
+        $temp['name'] = $row['name'];
+        $temp['branch_name'] = $row['branch_name'];
+        $sql = "SELECT COALESCE(SUM(amount),0) AS incentives FROM incentives WHERE staff_id = $staff_id AND YEAR(datetime) = YEAR('$date') AND WEEK(datetime) = WEEK('$date')";
+        $db->sql($sql);
+        $res = $db->getResult();
+        $temp['incentives'] = $res[0]['incentives'];
+
+
+        
+    
+        
+        $rows[] = $temp;
+    }
 
     $response['success'] = true;
     $response['message'] = "Incentive Details Fetched Successfully";
-    $response['data'] = $res;
+    $response['data'] = $rows;
     print_r(json_encode($response));
 
 } else {
